814abe92e8b44eeac8cdea7704585fcef573670d
Ulanbek <uislambek@gmail.com>
2025-08-03 00:34:39 +0600

    update config.py

--- config.py
+++ config.py
@@ -1,17 +1,20 @@
 import os
-from dotenv import load_dotenv
+import json
 
+CREDENTIALS_FILE = "google_credentials.json"
+json_data = os.getenv("GOOGLE_CREDENTIALS_JSON")
+
+if json_data:
+    with open(CREDENTIALS_FILE, "w") as f:
+        f.write(json_data)
+
+from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-
 OWNER_ID = int(os.getenv("OWNER_ID"))
 INVESTOR_ID = int(os.getenv("INVESTOR_ID"))
 
 GSHEET_NAME = os.getenv("GSHEET_NAME")
 GSHEET_TAB = os.getenv("GSHEET_TAB")
-
-GOOGLE_CREDENTIALS_PATH = os.getenv("GOOGLE_CREDENTIALS_PATH")
 GOOGLE_DRIVE_FOLDER_ID = os.getenv("GOOGLE_DRIVE_FOLDER_ID")
-
-GOOGLE_CREDENTIALS_JSON = os.getenv("GOOGLE_CREDENTIALS_JSON")
