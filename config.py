5a0114a53d9f05658ee271ecac244a2775b39d2c
Ulanbek <uislambek@gmail.com>
2025-08-03 00:28:22 +0600

    Remove unused config_example

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+OWNER_ID = int(os.getenv("OWNER_ID"))
+INVESTOR_ID = int(os.getenv("INVESTOR_ID"))
+
+GSHEET_NAME = os.getenv("GSHEET_NAME")
+GSHEET_TAB = os.getenv("GSHEET_TAB")
+
+GOOGLE_CREDENTIALS_PATH = os.getenv("GOOGLE_CREDENTIALS_PATH")
+GOOGLE_DRIVE_FOLDER_ID = os.getenv("GOOGLE_DRIVE_FOLDER_ID")
+
+GOOGLE_CREDENTIALS_JSON = os.getenv("GOOGLE_CREDENTIALS_JSON")
